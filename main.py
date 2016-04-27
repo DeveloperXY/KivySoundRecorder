0667d2850ce14189e57eaf08604887d1c2f4b00a
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-27 21:13:09 +0100

    Add an icon font.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.config import Config
+from kivy.core.text import LabelBase
 
 
 class RecorderApp(App):
@@ -9,5 +10,6 @@ class RecorderApp(App):
 if __name__ == '__main__':
     Config.set('graphics', 'width', 960)
     Config.set('graphics', 'height', 540)
+    LabelBase.register('Modern Pictograms', fn_regular='modernpics.ttf')
 
     RecorderApp().run()
